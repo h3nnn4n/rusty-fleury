a0975e9d13aa3fd33998fec3a6a7a28321ecb941
h3nnn4n <uber.renan@gmail.com>
2018-05-23 21:20:55 -0300

    Added complecity comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate text_io;
 
 fn vertex_degree_count_check(mat: &Vec<Vec<i32>>) -> bool {
+    // O(|V|)
+
     let mut n_odd = 0;
 
     for i in mat.iter() {
@@ -18,14 +20,20 @@ fn vertex_degree_count_check(mat: &Vec<Vec<i32>>) -> bool {
 }
 
 fn conectivity_test(mat: &Vec<Vec<i32>>, a: i32) -> bool {
+    // total: O(|V| + |A|)
+
     let mut visited: Vec<bool> = vec![false; mat.len()];
 
     dfs(mat, &mut visited, a);
+    // O(|V| + |A|)
 
     return all_visited(&visited);
+    // O(|V|)
 }
 
 fn all_visited(visited: &Vec<bool>) -> bool {
+    // O(|V|)
+
     for i in visited.iter() {
         if !i {
             return false;
@@ -36,22 +44,31 @@ fn all_visited(visited: &Vec<bool>) -> bool {
 }
 
 fn qntd_reachable_from(mat: &Vec<Vec<i32>>, a: i32) -> i32 {
+    // total: O(|V| + |A|)
+
     let mut visited: Vec<bool> = vec![false; mat.len()];
 
     dfs(mat, &mut visited, a);
+    // O(|V| + |A|)
 
     return visited.iter().map(|a| if *a { 1 } else { 0 }).sum();
+    // O(|V|)
 }
 
 fn dfs(mat: &Vec<Vec<i32>>, visited: &mut Vec<bool>, a: i32) {
+    // total: O(|V| + |A|)
+
     if all_visited(visited) {
+        // O(|V|)
         return;
     }
 
     for b in mat[a as usize].iter() {
+        // O(|A|)
         if !visited[*b as usize] {
             visited[*b as usize] = true;
             dfs(mat, visited, *b);
+            // O(|V|)
         }
     }
 }
@@ -65,21 +82,30 @@ fn fleury(mat: &mut Vec<Vec<i32>>, a: i32) -> Vec<i32> {
 }
 
 fn fleury_(mat: &mut Vec<Vec<i32>>, starting: i32, result: &mut Vec<i32>) {
+    // total: O(|V| * (|V| + |A|))
+
     let mut a = starting;
     let mut failsafe: bool;
     result.push(a);
 
     loop {
+        // O(|V|)
+
         failsafe = true;
         for i in 0..mat[a as usize].len() {
             let b = mat[a as usize][i];
             if valid_edge(mat, a, b) {
+                // O(|V| + |A|)
+
                 remove_edge_pair(mat, a, b);
+                // O(|A|);
+
                 a = b;
                 failsafe = false;
                 break;
             }
         }
+
         result.push(a);
 
         if a == starting {
@@ -94,24 +120,36 @@ fn fleury_(mat: &mut Vec<Vec<i32>>, starting: i32, result: &mut Vec<i32>) {
 }
 
 fn valid_edge(mat: &mut Vec<Vec<i32>>, a: i32, b: i32) -> bool {
+    // total: O(|V| + |A|)
+
     if mat[a as usize].len() == 1 {
         return true;
     } else {
         let q_before = qntd_reachable_from(mat, a);
+        // O(|V| + |A|)
+
         remove_edge_pair(mat, a, b);
+        // O(|A|);
+
         let q_after = qntd_reachable_from(mat, a);
+        // O(|V| + |A|)
+
         add_edge_pair(mat, a, b);
+        // O(1);
 
         return if q_before > q_after { false } else { true };
     }
 }
 
 fn add_edge_pair(mat: &mut Vec<Vec<i32>>, a: i32, b: i32) {
+    // O(1);
+
     mat[a as usize].push(b);
     mat[b as usize].push(a);
 }
 
 fn remove_edge_pair(mat: &mut Vec<Vec<i32>>, a: i32, b: i32) {
+    // O(|A|);
     let i1: usize = mat[a as usize].iter().position(|&r| r == b).unwrap();
     let i2: usize = mat[b as usize].iter().position(|&r| r == a).unwrap();
 
